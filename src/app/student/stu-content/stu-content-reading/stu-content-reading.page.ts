89b78fd956d488b23e387a2c3db28433375ad8fc
kayserjhamillex <anibaljhamil@gmail.com>
2022-11-18 09:59:39 -0500

    disenio comentario y final alternativo

--- src/app/student/stu-content/stu-content-reading/stu-content-reading.page.ts
+++ src/app/student/stu-content/stu-content-reading/stu-content-reading.page.ts
@@ -1,5 +1,12 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
+import { Libro } from 'src/app/models/libro';
+import { ParrafoService } from 'src/app/services/parrafo.service';
+import { LibroService } from 'src/app/services/libro.service';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import { ProgresoService } from 'src/app/services/progreso.service';
+import { Progreso } from 'src/app/models/progreso';
 
 @Component({
   selector: 'app-stu-content-reading',
@@ -7,17 +14,73 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./stu-content-reading.page.scss'],
 })
 export class StuContentReadingPage implements OnInit {
-  // <iframe width="560" height="315"
-  // src="https://www.youtube.com/embed/uNjrPgnI9rI"
-  // title="YouTube video player" frameborder="0"
-  //  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  //  allowfullscreen></iframe>
+  /* eslint-disable @typescript-eslint/naming-convention */
+  libro: Libro = {
+    id: 0,
+    Titulo: '',
+    Audio: '',
+    Video: '',
+    Imagen: '',
+    Autor: ''
+  };
+  progreso: Progreso = {
+    id: 0,
+    Progreso: '',
+    Reaccion: '',
+    Comentario: '',
+    FinalAlternativo: '',
+    FechaLectura: new Date(),
+    NotaCuestionario: '',
+    NumeroIntento: '',
+    LibroId: 0,
+    EstudianteId: 0
+  };
+  progreso1: Progreso = {
+    id: 0,
+    Progreso: '',
+    Reaccion: '',
+    Comentario: '',
+    FinalAlternativo: '',
+    FechaLectura: new Date(),
+    NotaCuestionario: '',
+    NumeroIntento: '',
+    LibroId: 0,
+    EstudianteId: 0
+  };
+  parrafos: any = [];
   elurl;
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
   constructor(
+    private router: Router,
     private sanitizer: DomSanitizer,
+    private datoService: DataService,
+    private libroService: LibroService,
+    private parrafoService: ParrafoService,
+    private progresoService: ProgresoService,
   ) { }
 
   ngOnInit() {
+    const parametro = JSON.parse(localStorage.getItem('ellibro'));
+    console.log(parametro);
+    this.libroService.getLibro(parametro).subscribe(
+      reslibro => {
+        this.libro = reslibro;
+
+
+        this.parrafoService.getsearchParrafobylibro(parametro).subscribe(
+          resparrafos => {
+            this.parrafos = resparrafos;
+          }, err => {
+            console.log('Error get parrafos by libro id');
+          }
+        );
+      }, err => {
+        console.log('Error get libro by id');
+      }
+    );
     this.elurl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/uNjrPgnI9rI');
   }
 
