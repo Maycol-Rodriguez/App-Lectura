d663c6915b68968474366b51b77d22034281cd7f
kordaya <74602531continental.edu.pe>
2022-11-19 11:48:47 -0500

    stu_content_video

--- src/app/student/stu-content/stu-content-video/stu-content-video.page.ts
+++ src/app/student/stu-content/stu-content-video/stu-content-video.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer } from '@angular/platform-browser';
+import { Libro } from 'src/app/models/libro';
+import { ParrafoService } from 'src/app/services/parrafo.service';
+import { LibroService } from 'src/app/services/libro.service';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import { ProgresoService } from 'src/app/services/progreso.service';
+import { Progreso } from 'src/app/models/progreso';
+import { EstudianteDetail } from 'src/app/models/estudiante';
 @Component({
   selector: 'app-stu-content-video',
   templateUrl: './stu-content-video.page.html',
@@ -7,9 +15,112 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StuContentVideoPage implements OnInit {
 
-  constructor() { }
+  
+/* eslint-disable @typescript-eslint/naming-convention */
+  
+progreso: Progreso = {
+  id: 0,
+  Progreso: '',
+  Reaccion: '',
+  Comentario: '',
+  FinalAlternativo: '',
+  FechaLectura: new Date(),
+  NotaCuestionario: '',
+  NumeroIntento: '',
+  LibroId: 0,
+  EstudianteId: 0
+};
+progreso1: Progreso = {
+  id: 0,
+  Progreso: '',
+  Reaccion: '',
+  Comentario: '',
+  FinalAlternativo: '',
+  FechaLectura: new Date(),
+  NotaCuestionario: '',
+  NumeroIntento: '',
+  LibroId: 0,
+  EstudianteId: 0
+};
+estudiante: EstudianteDetail = {
+  id: 0,
+  Nombre: '',
+  Apellido: '',
+  Genero: '',
+  Documento: '',
+  GradoId: 0,
+  grado: {
+    id: 0,
+    Nombre: '',
+  },
+};
+parrafos: any = [];
+elurl;
+existencia = 'no existe';
+// en progreso
+// finalizado
+constructor(
+  private router: Router,
+  private sanitizer: DomSanitizer,
+  private datoService: DataService,
+  private libroService: LibroService,
+  private parrafoService: ParrafoService,
+  private progresoService: ProgresoService,
+) { }
 
-  ngOnInit() {
-  }
+ngOnInit() {
+  const parametro = JSON.parse(localStorage.getItem('ellibro'));
+  const parestudiante = JSON.parse(localStorage.getItem('usuario'));
+  console.log(parametro);
+  this.libroService.getLibro(parametro).subscribe(
+    reslibro => {
+      //this.libro = reslibro;
+      // sacar el progreso mediante libro y estudiante
+      this.parrafoService.getsearchParrafobylibro(parametro).subscribe(
+        resparrafos => {
+          this.parrafos = resparrafos;
+        }, err => {
+          console.log('Error get parrafos by libro id');
+        }
+      );
+    }, err => {
+      console.log('Error get libro by id');
+    }
+  );
+  this.elurl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/LzMKOMwakVA');
+}
+elegir(dato) {
+  this.progreso.Reaccion = dato;
+}
+deselegir() {
+  this.progreso.Reaccion = '';
+}
+validarprogreso(dato) {
+  // progreso completado
+  // progreso existente
+  // no existe progreso => crear
+}
+actualizar(dato) {
+  this.validarprogreso(dato);
+  // se valida primero
+  // se actualiza el progreso existente
+  // se actualiza en completado solo el comentario y el final alternativo
+}
+iracuestionario(dato) {
+  this.actualizar(dato);
+  this.router.navigate(
+    [
+      'student',
+      'stu-content',
+      'stu-content-evaluation'
+    ]
+  );
+  // actualizamos el progreso
+  // lo mandamos al cuestionario
+}
 
+terminar(dato) {
+  // actualizamos el progreso
+  // lo mandamos al home
+}
 }
