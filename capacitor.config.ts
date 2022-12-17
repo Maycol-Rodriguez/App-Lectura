4d70006688b6c33614fd2ef067748ad9555c8c1c
kayserjhamillex <anibaljhamil@gmail.com>
2022-12-16 19:22:49 -0500

    generando android proyect

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,29 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+  // appId: 'com.jxdesign.lecapp',
   appId: 'io.ionic.starter',
   appName: 'applectura-ionic6-angular14',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      // backgroundColor: '#ffffffff',
+      androidSplashResourceName: 'splash',
+      // androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      // androidSpinnerStyle: 'large',
+      // iosSpinnerStyle: 'small',
+      // spinnerColor: '#999999',
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
